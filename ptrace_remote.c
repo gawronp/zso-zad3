55814eb9071b8fe687295d416d026badedeef4f1
Piotr Gawryluk <pgawryluk0@gmail.com>
2018-06-14 17:16:57 +0200

    Last one, I hope.

--- ptrace_remote.c
+++ ptrace_remote.c
@@ -13,49 +13,14 @@
 #include <linux/slab.h>
 #include <linux/fdtable.h>
 
-static int remote_mmap(struct ptrace_remote_task *task_data);
-static int remote_munmap(struct ptrace_remote_task *task_data);
-static int remote_mremap(struct ptrace_remote_task *task_data);
-static int remote_mprotect(struct ptrace_remote_task *task_data);
-static int remote_dup(struct ptrace_remote_task *task_data);
-static int remote_dup2(struct ptrace_remote_task *task_data);
-static void dup_from_remote(struct ptrace_remote_task *task_data);
-static int remote_close(struct ptrace_remote_task *task_data);
-
-static void remote_task_function(void)
-{
-	struct ptrace_remote_task *defered_task = current->ptrace_defered_task;
-	switch (defered_task->request) {
-	case PTRACE_REMOTE_MMAP:
-		defered_task->retval = remote_mmap(defered_task);
-		break;
-	case PTRACE_REMOTE_MUNMAP:
-		defered_task->retval = remote_munmap(defered_task);
-		break;
-	case PTRACE_REMOTE_MREMAP:
-		defered_task->retval = remote_mremap(defered_task);
-		break;
-	case PTRACE_REMOTE_MPROTECT:
-		defered_task->retval = remote_mprotect(defered_task);
-		break;
-	case PTRACE_DUP_TO_REMOTE:
-		defered_task->retval = remote_dup(defered_task);
-		break;
-	case PTRACE_DUP2_TO_REMOTE:
-		defered_task->retval = remote_dup2(defered_task);
-		break;
-	case PTRACE_DUP_FROM_REMOTE:
-		dup_from_remote(defered_task);
-		break;
-	case PTRACE_REMOTE_CLOSE:
-		defered_task->retval = remote_close(defered_task);
-		break;
-	default:
-		BUG_ON("remote_task_function called with unknown request code - this should never happen!\n");
-	}
-	set_current_state(defered_task->child_orig_state);
-	complete(&defered_task->remote_completion);
-}
+static void remote_mmap(void);
+static void remote_munmap(void);
+static void remote_mremap(void);
+static void remote_mprotect(void);
+static void remote_dup(void);
+static void remote_dup2(void);
+static void dup_from_remote(void);
+static void remote_close(void);
 
 int ptrace_request_remote(struct task_struct *child, long request, unsigned long addr, unsigned long data)
 {
@@ -66,11 +31,11 @@ int ptrace_request_remote(struct task_struct *child, long request, unsigned long
 		return -ENOMEM;
 
 	child->ptrace_defered_task->request = request;
-	child->ptrace_defered_task->defered_fun = remote_task_function;
 	init_completion(&child->ptrace_defered_task->remote_completion);
 
 	switch (request) {
 	case PTRACE_REMOTE_MMAP:
+		child->ptrace_defered_task->defered_fun = remote_mmap;
 		child->ptrace_defered_task->data_from_user = kzalloc(sizeof(struct ptrace_remote_mmap), GFP_KERNEL);
 		if (unlikely(!child->ptrace_defered_task->data_from_user)) {
 			ret = -ENOMEM;
@@ -126,6 +91,7 @@ int ptrace_request_remote(struct task_struct *child, long request, unsigned long
 		break;
 
 	case PTRACE_REMOTE_MUNMAP:
+		child->ptrace_defered_task->defered_fun = remote_munmap;
 		child->ptrace_defered_task->data_from_user = kzalloc(sizeof(struct ptrace_remote_munmap), GFP_KERNEL);
 		if (unlikely(!child->ptrace_defered_task->data_from_user)) {
 			ret = -ENOMEM;
@@ -148,6 +114,7 @@ int ptrace_request_remote(struct task_struct *child, long request, unsigned long
 		break;
 
 	case PTRACE_REMOTE_MREMAP:
+		child->ptrace_defered_task->defered_fun = remote_mremap;
 		child->ptrace_defered_task->data_from_user = kzalloc(sizeof(struct ptrace_remote_mremap), GFP_KERNEL);
 		if (unlikely(!child->ptrace_defered_task->data_from_user)) {
 			ret = -ENOMEM;
@@ -178,6 +145,7 @@ int ptrace_request_remote(struct task_struct *child, long request, unsigned long
 		break;
 
 	case PTRACE_REMOTE_MPROTECT:
+		child->ptrace_defered_task->defered_fun = remote_mprotect;
 		child->ptrace_defered_task->data_from_user = kzalloc(sizeof(struct ptrace_remote_mprotect), GFP_KERNEL);
 		if (unlikely(!child->ptrace_defered_task->data_from_user)) {
 			ret = -ENOMEM;
@@ -205,6 +173,7 @@ int ptrace_request_remote(struct task_struct *child, long request, unsigned long
 		break;
 
 	case PTRACE_DUP_TO_REMOTE:
+		child->ptrace_defered_task->defered_fun = remote_dup;
 		child->ptrace_defered_task->data_from_user = kzalloc(sizeof(struct ptrace_dup_to_remote), GFP_KERNEL);
 		if (unlikely(!child->ptrace_defered_task->data_from_user)) {
 			ret = -ENOMEM;
@@ -240,6 +209,7 @@ int ptrace_request_remote(struct task_struct *child, long request, unsigned long
 		break;
 
 	case PTRACE_DUP2_TO_REMOTE:
+		child->ptrace_defered_task->defered_fun = remote_dup2;
 		child->ptrace_defered_task->data_from_user = kzalloc(sizeof(struct ptrace_dup2_to_remote), GFP_KERNEL);
 		if (unlikely(!child->ptrace_defered_task->data_from_user)) {
 			ret = -ENOMEM;
@@ -275,6 +245,7 @@ int ptrace_request_remote(struct task_struct *child, long request, unsigned long
 		break;
 
 	case PTRACE_DUP_FROM_REMOTE:
+		child->ptrace_defered_task->defered_fun = dup_from_remote;
 		child->ptrace_defered_task->data_from_user = kzalloc(sizeof(struct ptrace_dup_from_remote), GFP_KERNEL);
 		if (unlikely(!child->ptrace_defered_task->data_from_user)) {
 			ret = -ENOMEM;
@@ -309,6 +280,7 @@ int ptrace_request_remote(struct task_struct *child, long request, unsigned long
 		break;
 
 	case PTRACE_REMOTE_CLOSE:
+		child->ptrace_defered_task->defered_fun = remote_close;
 		child->ptrace_defered_task->data_from_user = kzalloc(sizeof(struct ptrace_remote_close), GFP_KERNEL);
 		if (unlikely(!child->ptrace_defered_task->data_from_user)) {
 			ret = -ENOMEM;
@@ -343,75 +315,108 @@ int ptrace_request_remote(struct task_struct *child, long request, unsigned long
 	return ret;
 }
 
-static int remote_mmap(struct ptrace_remote_task *task_data)
+static void finish_defered_fun(void)
 {
-	uint64_t retval;
+	set_current_state(current->ptrace_defered_task->child_orig_state);
+	complete(&current->ptrace_defered_task->remote_completion);
+}
+
+static void remote_mmap(void)
+{
+	unsigned long retval;
 	unsigned long populate = 0;
+	struct ptrace_remote_task *task_data = current->ptrace_defered_task;
 	struct ptrace_remote_mmap *mmap_data = (struct ptrace_remote_mmap *) task_data->data_from_user;
 
-	if (down_write_killable(&current->mm->mmap_sem))
-		return -EINTR;
+	if (down_write_killable(&current->mm->mmap_sem)) {
+		current->ptrace_defered_task->retval = -EINTR;
+		goto mmap_return;
+	}
 
 	retval = do_mmap(task_data->file_ptr, mmap_data->addr, mmap_data->length, mmap_data->prot,
 		mmap_data->flags, 0, mmap_data->offset / PAGE_SIZE, &populate);
 
 	up_write(&current->mm->mmap_sem);
 
-	if (IS_ERR_VALUE(retval))
-		return retval;
+	if (IS_ERR_VALUE(retval)) {
+		current->ptrace_defered_task->retval = retval;
+		goto mmap_return;
+	}
 
 	mmap_data->addr = retval;
 
 	if (populate > 0)
 		mm_populate(retval, populate);
 
-	return 0;
+	current->ptrace_defered_task->retval = 0;
+
+mmap_return:
+	finish_defered_fun();
 }
 
-static int remote_munmap(struct ptrace_remote_task *task_data)
+static void remote_munmap(void)
 {
-	return sys_munmap(((struct ptrace_remote_munmap * ) task_data->data_from_user)->addr,
+	struct ptrace_remote_task *task_data = current->ptrace_defered_task;
+	task_data->retval = sys_munmap(((struct ptrace_remote_munmap * ) task_data->data_from_user)->addr,
 		((struct ptrace_remote_munmap * ) task_data->data_from_user)->length);
+	finish_defered_fun();
 }
 
-static int remote_mremap(struct ptrace_remote_task *task_data)
+static void remote_mremap(void)
 {
+	struct ptrace_remote_task *task_data = current->ptrace_defered_task;
 	struct ptrace_remote_mremap *remap_data = (struct ptrace_remote_mremap *) task_data->data_from_user;
 
 	uint64_t new_addr = sys_mremap(remap_data->old_addr, remap_data->old_size, remap_data->new_size,
 		remap_data->flags, remap_data->new_addr);
 
-	if (IS_ERR_VALUE(new_addr))
-		return new_addr;
+	if (IS_ERR_VALUE(new_addr)) {
+		task_data->retval = new_addr;
+		goto mremap_return;	
+	}
 
 	remap_data->new_addr = new_addr;
-	return 0;
+	task_data->retval = 0;
+
+mremap_return:
+	finish_defered_fun();
 }
 
-static int remote_mprotect(struct ptrace_remote_task *task_data)
+static void remote_mprotect(void)
 {
-	return sys_mprotect(((struct ptrace_remote_mprotect *) task_data->data_from_user)->addr,
+	struct ptrace_remote_task *task_data = current->ptrace_defered_task;
+	task_data->retval = sys_mprotect(((struct ptrace_remote_mprotect *) task_data->data_from_user)->addr,
 		((struct ptrace_remote_mprotect *) task_data->data_from_user)->length,
 		((struct ptrace_remote_mprotect *) task_data->data_from_user)->prot);
+	finish_defered_fun();
 }
 
-static int remote_dup(struct ptrace_remote_task *task_data)
+static void remote_dup(void)
 {
-	return f_dupfd(0, task_data->file_ptr, ((struct ptrace_dup_to_remote *) task_data->data_from_user)->flags);
+	struct ptrace_remote_task *task_data = current->ptrace_defered_task;
+	task_data->retval = f_dupfd(0, task_data->file_ptr,
+		((struct ptrace_dup_to_remote *) task_data->data_from_user)->flags);
+	finish_defered_fun();
 }
 
-static int remote_dup2(struct ptrace_remote_task *task_data)
+static void remote_dup2(void)
 {
-	return replace_fd(((struct ptrace_dup2_to_remote *) task_data->data_from_user)->remote_fd, task_data->file_ptr,
-		((struct ptrace_dup2_to_remote *) task_data->data_from_user)->flags);
+	struct ptrace_remote_task *task_data = current->ptrace_defered_task;
+	task_data->retval = replace_fd(((struct ptrace_dup2_to_remote *) task_data->data_from_user)->remote_fd,
+		task_data->file_ptr, ((struct ptrace_dup2_to_remote *) task_data->data_from_user)->flags);
+	finish_defered_fun();
 }
 
-static void dup_from_remote(struct ptrace_remote_task *task_data)
+static void dup_from_remote(void)
 {
+	struct ptrace_remote_task *task_data = current->ptrace_defered_task;
 	task_data->file_ptr = fget(((struct ptrace_dup_from_remote *) task_data->data_from_user)->remote_fd);
+	finish_defered_fun();
 }
 
-static int remote_close(struct ptrace_remote_task *task_data)
+static void remote_close(void)
 {
-	return sys_close(((struct ptrace_remote_close *) task_data->data_from_user)->remote_fd);
+	struct ptrace_remote_task *task_data = current->ptrace_defered_task;
+	task_data->retval = sys_close(((struct ptrace_remote_close *) task_data->data_from_user)->remote_fd);
+	finish_defered_fun();
 }
